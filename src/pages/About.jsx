b209d4ea8a3eb8a5cddb9ea5d737bf2b3e966039
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2022-07-09 20:25:56 +0900

    0709

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@material-ui/core";
+
+
+export default function About() {
+    return (
+        <Box>
+            <div>
+                About
+            </div>
+        </Box>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@material-ui/core";
+import Room from "../components/Room";
+
+export default function Home() {
+    return (
+        <Box>
+            <div>
+                Hello
+            </div>
+            
+            <Room />
+        </Box>
+    )
+}
